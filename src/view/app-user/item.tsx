0a41db101b4e93993c2e4f58fae6fb7fc13d2352
zhanchao.wu <zhanchao.wu@icloud.com>
2022-05-15 11:35:35 +0800

    feat(v2.0): v2.0

--- src/view/app-user/item.tsx
+++ src/view/app-user/item.tsx
@@ -1,4 +1,7 @@
-import SchemaForm, { SchemaFType } from '@/components/schema-control/schema-form';
+import SchemaForm, { SchemaFormType, SchemaFType } from '@/components/schema-control/schema-form';
+import service from '@/utils/axios-helper';
+import { Maybe } from '@/utils/ts-helper';
+import { message } from 'ant-design-vue';
 import { defineComponent, FunctionalComponent, reactive, ref } from 'vue';
 import { useRequest } from 'vue-request';
 import { useRoute } from 'vue-router';
@@ -10,6 +13,18 @@ const rules = {
 };
 
 /**
+ * 保存
+ * @param value
+ */
+const runSave = (value: any, id?: Maybe<string>) => {
+  debugger;
+  if (id) {
+    return service.put(`/api/app-user/${id}`, value);
+  }
+  return service.post('/api/app-user', value);
+};
+
+/**
  * 用户编辑
  * @returns
  */
@@ -17,41 +32,79 @@ const Item: FunctionalComponent = () => {
   const { id } = useRoute().params;
   const { data, loading, error, run } = useRequest(queryData, {
     formatResult: (res) => res.data,
-    manual: true,
+    manual: !id,
   });
-  const { run: mutationRun, loading: mutationLoading } = useRequest(mutatioinSave, {
+  const { run: mutationRun, loading: mutationLoading } = useRequest(runSave, {
     manual: true,
   });
 
   const handleFinish = (value: FormState) => {
-    mutatioinSave(value);
+    mutationRun(value, id as string).then((res) => {
+      message.success('保存成功！');
+    });
   };
 
   const schema = reactive<SchemaFType<FormState>>({
     formName: 'appUsers',
-    initValue: data.value,
+    initValue: (data.value || {
+      phone: '',
+      code: '',
+      realname: '',
+      nickname: '',
+    }) as FormState,
     ruleRefs: rules,
     formItems: [
       {
         controlType: 'Input',
         keyId: 'phone',
+        label: '手机号',
+        controlPropery: {
+          allowClear: true,
+        },
       },
       {
         controlType: 'Input',
         keyId: 'code',
+        label: '编号',
+        controlPropery: {
+          allowClear: true,
+        },
       },
       {
         controlType: 'Input',
         keyId: 'realname',
+        label: '真实姓名',
+        controlPropery: {
+          allowClear: true,
+        },
       },
       {
         controlType: 'Input',
         keyId: 'nickname',
+        label: '昵称',
+        controlPropery: {
+          allowClear: true,
+        },
+      },
+      {
+        controlType: 'SubmitRest',
       },
     ],
   });
 
-  return () => <SchemaForm schema={schema as any} onHandleFinish={handleFinish}></SchemaForm>;
+  /**
+   * 表单 ref
+   */
+  const schemaFromRef = ref<Maybe<SchemaFormType>>(null);
+
+  return () => (
+    <SchemaForm
+      schema={schema as any}
+      onHandleFinish={handleFinish}
+      transitionLoadProps={{ spinning: loading.value }}
+      ref={schemaFromRef}
+    ></SchemaForm>
+  );
 };
 export type ItemType = typeof Item;
 export default defineComponent(Item);
